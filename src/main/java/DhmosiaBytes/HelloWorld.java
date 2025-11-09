5441aee4861fadf5ce16a1491d3e50e8c81e6dc5
MyrtoNtokou <t8240244@aueb.gr>
2025-11-09 13:33:07 +0200

    Added  HelloWorld class and test for Maven setup

---
+++ src/main/java/DhmosiaBytes/HelloWorld.java
@@ -0,0 +1,11 @@
+package DhmosiaBytes;
+
+public class HelloWorld {
+    public static String sayHello() {
+        return "Hello, world!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+}
---
+++ src/test/java/DhmosiaBytes/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package DhmosiaBytes;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestHelloWorld {
+
+    @Test
+    public void testSayHello() {
+        assertEquals("Hello, world!", HelloWorld.sayHello());
+    }
+}
