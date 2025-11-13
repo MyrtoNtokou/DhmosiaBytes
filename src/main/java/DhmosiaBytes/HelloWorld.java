627c8cd5e4351aec0b024fd2d4c708204749beae
MyrtoNtokou <t8240244@aueb.gr>
2025-11-13 21:39:02 +0200

    File corrections and added package info

--- src/main/java/DhmosiaBytes/HelloWorld.java
+++ src/main/java/DhmosiaBytes/HelloWorld.java
@@ -1,11 +1,30 @@
-package DhmosiaBytes;
+package dhmosiabytes;
 
-public class HelloWorld {
+/**
+ * Utility class that provides a greeting.
+ */
+public final class HelloWorld {
+
+    // Hide constructor for utility class
+    private HelloWorld() {
+        throw new AssertionError("No instances.");
+    }
+
+    /**
+     * Returns the hello message.
+     *
+     * @return the hello message
+     */
     public static String sayHello() {
         return "Hello, world!";
     }
 
-    public static void main(String[] args) {
+    /**
+     * Entry point.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args) {
         System.out.println(sayHello());
     }
 }
--- src/test/java/DhmosiaBytes/TestHelloWorld.java
+++ src/test/java/DhmosiaBytes/TestHelloWorld.java
@@ -1,4 +1,4 @@
-package DhmosiaBytes;
+package dhmosiabytes;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
