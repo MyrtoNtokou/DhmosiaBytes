941c9e204b3f3d201890171352fa3ef9c99d5dc4
MyrtoNtokou <t8240244@aueb.gr>
2025-11-20 23:18:43 +0200

    Managed Exceptions

--- src/main/java/DhmosiaBytes/PdfDownloader.java
+++ src/main/java/DhmosiaBytes/PdfDownloader.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import java.net.URL;
+import java.net.MalformedURLException;
 
 import java.nio.file.Path;
 import java.nio.file.Files;
@@ -25,7 +26,7 @@ public final class PdfDownloader {
      * @param year
      * @throws IOException for error during download or save
      */
-    public static void downloadPdf(final int year) throws IOException {
+    public static void downloadPdf(final int year) {
         // Saves the link of the pdf
         String fileUrl = BudgetUrlFinder.findUrl(year);
         // Checks that the url is not null
@@ -34,20 +35,32 @@ public final class PdfDownloader {
             return;
         }
         // Saves the link in a URL object
-        URL url = new URL(fileUrl);
+        URL url = null;
+        try {
+            url = new URL(fileUrl);
+        } catch (MalformedURLException e) {
+            System.err.println("Σφάλμα κατά τον εντοπισμό του αρχείου");
+            return;
+        }
         // Sets the name of the pdf that will be downloaded
         String fileName = "budget-" + year + ".pdf";
         // Creates the path of the folder where the pdfs will be saved
         Path budgetsDir = Path.of("budgetsInPdf");
         // Creates the folder if it does not exist
         if (!Files.exists(budgetsDir)) {
-            Files.createDirectories(budgetsDir);
+            try {
+                Files.createDirectories(budgetsDir);
+            } catch (IOException e) {
+                System.err.println("Δεν ήταν δυνατή η δημιουργία του φακέλου.");
+            }
         }
         // Creates the full path of the pdf
         Path destinationPath = budgetsDir.resolve(fileName);
         try (InputStream in = url.openStream()) {
             Files.copy(in, destinationPath,
                 StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.err.println("Σφάλμα κατά την αποθήκευση του αρχείου");
         }
     }
 
@@ -57,12 +70,16 @@ public final class PdfDownloader {
      * @param year
      * @throws IOException for error during delete
      */
-    public static void deletePdf(final int year) throws IOException {
+    public static void deletePdf(final int year) {
         // Creates the path of the folder with the pdfs
         Path budgetsDir = Path.of("budgetsInPdf");
         // Creates the path with the pdf that will be deleted
         Path pdfFile = budgetsDir.resolve("budget-" + year + ".pdf");
         // Deletes the pdf
-        Files.deleteIfExists(pdfFile);
+        try {
+            Files.deleteIfExists(pdfFile);
+        } catch (IOException e) {
+            System.err.println("Σφάλμα κατά την διαγραφή του αρχείου");
+        }
     }
 }
