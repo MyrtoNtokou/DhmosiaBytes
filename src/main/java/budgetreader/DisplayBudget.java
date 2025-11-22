d8a75e629a118cf019f859945d39ed6bc93e3709
Ilias Pavlidis <t8240112@aueb.gr>
2025-11-22 12:13:19 +0000

    Add java comments and alter display methods structure

--- src/main/java/budgetreader/DisplayBudget.java
+++ src/main/java/budgetreader/DisplayBudget.java
@@ -2,21 +2,33 @@ package budgetreader;
 
 import java.util.List;
 
+/**
+ * Utility class for displaying budget data.
+ */
 public final class DisplayBudget {
-    
+   
+    /**
+     * Private constructor to prevent object creation.
+     */
     private DisplayBudget() {}
 
-    public static <T> void displayList(List<T> list) {
-        list.forEach(System.out::println);
-    }
-
+    /** Prints the General Budget List on the screen */
     public static void showGeneral(List<Eggrafi> eggrafes) {
         System.out.println("\n=== ΓΕΝΙΚΟΣ ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ ===");
-        displayList(eggrafes);
+        
+        /* Print each record in the list */
+        for (Eggrafi e : eggrafes) {
+            System.out.println(e);
+        }
     }
 
+    /** Prints the Ministry Budget List on the screen */
     public static void showMinistry(List<Ypourgeio> ypourg) {
         System.out.println("\n=== ΠΡΟΫΠΟΛΟΓΙΣΜΟΣ ΑΝΑ ΥΠΟΥΡΓΕΙΟ ===");
-        displayList(ypourg);
+        
+        /* Print each record in the list*/
+        for (Ypourgeio y : ypourg) {
+            System.out.println(y);
+        }
     }
 }
