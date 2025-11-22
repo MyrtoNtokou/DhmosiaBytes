8a4d04807e82672c3c58adafd6048c47d865a784
Ilias Pavlidis <t8240112@aueb.gr>
2025-11-22 14:32:13 +0000

    Change the path from csv file

--- src/main/java/budgetreader/ReadBudget.java
+++ src/main/java/budgetreader/ReadBudget.java
@@ -1,36 +1,50 @@
 package budgetreader;
 
-/**Utility class that reads budget data from CSV files and processes
- * them according to the application's requirements.*/
-
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
-
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReadBudget {
+/**Utility class that reads budget data from CSV files and processes
+ * them according to the application's requirements.*/
+
+public final class ReadBudget {
     /**
     * Private constructor to prevent instantiation.
     */
-    private ReadBudget() {
-        throw new AssertionError("Utility class should not be instantiated.");
-    }
+    private ReadBudget() { }
 
-    /** Reads output.csv from local file system
-     *  and converts each row into an @Eggrafi object 
+    /** Reads the proper csv file
+    * from folder resources and converts each row into an @Eggrafi object
      */ 
-        public static List<Eggrafi> readGeneralBudget(String filePath) {
+        public static List<Eggrafi> readGeneralBudget(String resourceName) {
 
         List<Eggrafi> eggrafes = new ArrayList<>();
+        try {
+            /* Loads CSV file from classpath (resources folder) */
+            InputStream input = ReadBudget.class.getResourceAsStream(
+                "/" + resourceName);
 
-        try (var reader = new CSVReaderBuilder(
-            new InputStreamReader(
-                new java.io.FileInputStream(filePath),
-                StandardCharsets.UTF_8
-            )).build()) {
+                if (input == null) {
+                    System.err.println(
+                        "❌ Δεν βρέθηκε το αρχείο: " + resourceName);
+                    return eggrafes;
+                }
+                
+            /*Converts delimiter to ";" instead of "," */    
+            CSVParser parser = new CSVParserBuilder()
+                .withSeparator(';')
+                    .build();
+
+            CSVReader reader = new CSVReaderBuilder(
+                new InputStreamReader(input, StandardCharsets.UTF_8)
+                    ).withCSVParser(parser)
+                        .build();
 
             String[] line;
 
@@ -40,36 +54,52 @@ public class ReadBudget {
                 String kodikos = line[0].trim();
                 String perigrafi = line[1].trim();
                 double poso = parseNumber(line[2].trim());
-
                 eggrafes.add(new Eggrafi(kodikos, perigrafi, poso));
             }
         } catch (Exception e) {
-            System.err.println("Δεν μπόρεσα να διαβάσω το αρχείο: " 
-                + filePath);
             e.printStackTrace();
         }
         return eggrafes;
     }
 
-    /** Reads output.csv from local file system
+    /** Reads cσv file from folder resources
      *  and converts each row into an @Ypourgeio object 
      */ 
-    public static List<Ypourgeio> readByMinistry(String filePath) {
+    public static List<Ypourgeio> readByMinistry(String resourceName) {
 
         List<Ypourgeio> ypourg = new ArrayList<>();
-        try (var reader = new CSVReaderBuilder(
-        new InputStreamReader(
-            new FileInputStream(filePath),
-            StandardCharsets.UTF_8
-        )).build()) {
-            
+        try {
+            /* Loads  CSV file from classpath (resources folder) */
+            InputStream input = ReadBudget.class.getResourceAsStream(
+                "/" + resourceName);
+            if (input == null) {
+                System.err.println("❌ Δεν βρέθηκε το αρχείο: " + resourceName);
+                return ypourg;
+            }
+            /*Converts delimiter to ";" instead of "," */
+            CSVParser parser = new CSVParserBuilder()
+                .withSeparator(';')
+                    .build();
+
+            CSVReader reader = new CSVReaderBuilder(
+                new InputStreamReader(input, StandardCharsets.UTF_8)
+                    ).withCSVParser(parser)
+                        .build();
             String[] line;
 
             /* Creates new instances Ypourgeio for each line */
             while ((line = reader.readNext()) != null) {
                 if (line.length < 5) continue;
                 try {
-                    int kodikos = Integer.parseInt(line[0].trim());
+                    String kodikosStr = line[0].trim().replace("\uFEFF", "");
+
+                /* If it's not a number  → skip */
+                    if (!kodikosStr.matches("\\d+")) {
+                        continue;
+                    }
+
+                    int kodikos = Integer.parseInt(kodikosStr);
+
                     String onoma = line[1].trim();
                     double taktikos = parseNumber(line[2]);
                     double ependyseis = parseNumber(line[3]);
@@ -77,14 +107,14 @@ public class ReadBudget {
                     ypourg.add(new Ypourgeio(
                         kodikos, onoma, taktikos, ependyseis, synolo));
                 } catch (Exception e) {
-                        System.err.println("Skipped invalid CSV line: " 
+                        System.err.println("Προσπέραση λανθασμένης εγγραφής: " 
                         + e.getMessage());
 
                 }
             }
         } catch (Exception e) {
             System.err.println("Δεν μπόρεσα να διαβάσω το αρχείο: " 
-                + filePath);
+                + resourceName);
             e.printStackTrace();
         }
         return ypourg;
