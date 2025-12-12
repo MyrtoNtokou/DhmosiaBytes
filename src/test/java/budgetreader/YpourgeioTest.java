39d0b20a98f9f44c14ca811c30fe4f32798513ad
MyrtoNtokou <t8240244@aueb.gr>
2025-12-12 18:30:43 +0200

    Correct for Ypourgeio

--- src/test/java/budgetreader/YpourgeioTest.java
+++ src/test/java/budgetreader/YpourgeioTest.java
@@ -17,15 +17,15 @@ public class YpourgeioTest {
     @Test
     public void testConstructorAndGetters() {
         Ypourgeio y = new Ypourgeio(10, "Test Ministry",
-                new BigDecimal(50.0),
-                new BigDecimal(30.0),
-                new BigDecimal(80.0));
+                new BigDecimal("50.0"),
+                new BigDecimal("30.0"),
+                new BigDecimal("80.0"));
 
         assertEquals(10, y.getKodikos());
         assertEquals("Test Ministry", y.getOnoma());
-        assertEquals(50.0, y.getTaktikos());
-        assertEquals(30.0, y.getEpendyseis());
-        assertEquals(80.0, y.getSynolo());
+        assertEquals(new BigDecimal("50.0"), y.getTaktikos());
+        assertEquals(new BigDecimal("30.0"), y.getEpendyseis());
+        assertEquals(new BigDecimal("80.0"), y.getSynolo());
     }
 
     /**
@@ -33,8 +33,8 @@ public class YpourgeioTest {
      */
     @Test
     public void testToString() {
-        Ypourgeio y = new Ypourgeio(1, "X", new BigDecimal(10.0), 
-        new BigDecimal(5.0), new BigDecimal(15.0));
+        Ypourgeio y = new Ypourgeio(1, "X", new BigDecimal("10.0"), 
+        new BigDecimal("5.0"), new BigDecimal("15.0"));
 
         String text = y.toString();
 
