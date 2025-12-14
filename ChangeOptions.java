ec9a17533ca816b48bd47e34a21a9428035e52f2
Theodora Kaltsa <t8240058@aueb.gr>
2025-12-14 17:55:09 +0200

    Correct BudgetChange

--- ChangeOptions.java
+++ ChangeOptions.java
@@ -1,20 +1,23 @@
 package dhmosiabytes;
 
     public enum  ChangeOptions{
-        INCOME(1 ,"Έσοδα Προϋπολογισμού"),
-        EXPENSE(2,"Έξοδα Προϋπολογισμού");
+        INCOME(1 , "Έσοδα Προϋπολογισμού"),
+        EXPENSE(2, "Έξοδα Προϋπολογισμού");
     private final int changeCode;
     private final String changeDescription;
     ChangeOptions(final int givenChangeCode, final String givenChangeDescription) {
         this.changeCode = givenChangeCode;
         this.changeDescription = givenChangeDescription;
     }
+
     public int getChangeCode() {
         return this.changeCode;
     }
+
     public String getChangeDescription() {
         return changeDescription;
     }
+
     public static ChangeOptions CodeOption(final int changeCode) {
         for (ChangeOptions changeOpt : ChangeOptions.values()) {
             if (changeOpt.getChangeCode() == changeCode) {
---
+++ src/main/java/DhmosiaBytes/ChangeOptions.java
@@ -0,0 +1,27 @@
+package dhmosiabytes; 
+
+public enum  ChangeOptions {
+    INCOME(1 ,"Έσοδα Προϋπολογισμού"),
+    EXPENSE(2,"Έξοδα Προϋπολογισμού");
+    private final int changeCode;
+    private final String changeDescription;
+
+    ChangeOptions(final int givenChangeCode, final String givenChangeDescription) {
+        this.changeCode = givenChangeCode;
+        this.changeDescription = givenChangeDescription;
+    }
+    public int getChangeCode() {
+        return this.changeCode;
+    }
+    public String getChangeDescription() {
+        return changeDescription;
+    }
+    public static ChangeOptions CodeOption(final int changeCode) {
+        for (ChangeOptions changeOpt : ChangeOptions.values()) {
+            if (changeOpt.getChangeCode() == changeCode) {
+                return changeOpt;
+            }
+        }
+        return null;
+    }
+}
