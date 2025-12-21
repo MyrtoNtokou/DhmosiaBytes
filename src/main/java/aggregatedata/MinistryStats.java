2fc594945ee19485d96fcd4dc20230eedc0c15d1
MyrtoNtokou <t8240244@aueb.gr>
2025-12-21 17:04:05 +0200

    Add percentage fields for ministries

--- src/main/java/aggregatedata/MinistryStats.java
+++ src/main/java/aggregatedata/MinistryStats.java
@@ -2,6 +2,7 @@ package aggregatedata;
 
 import budgetreader.Ypourgeio;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,39 +14,40 @@ public final class MinistryStats {
 
     /** Max Taktikos list initialisation. */
     private final List<Ypourgeio> maxTaktikos;
-    /** Min Taktikos list initialisation. */
-    private final List<Ypourgeio> minTaktikos;
     /** Max Ependyseis list initialisation. */
     private final List<Ypourgeio> maxEpendyseis;
-    /** Min Ependyseis list initialisation. */
-    private final List<Ypourgeio> minEpendyseis;
     /** Max Synolo list initialisation. */
     private final List<Ypourgeio> maxSynolo;
-    /** Min Synolo list initialisation. */
-    private final List<Ypourgeio> minSynolo;
+    /** Max Taktikos percentages list initialisation. */
+    private final List<BigDecimal> maxTaktikosPercentages;
+    /** Max Ependyseis list initialisation. */
+    private final List<BigDecimal> maxEpendyseisPercentages;
+    /** Max Synolo list initialisation. */
+    private final List<BigDecimal> maxSynoloPercentages;
+
 
     /** Constructor.
      * @param maxTakt
-     * @param minTakt
      * @param maxEpend
-     * @param minEpend
      * @param maxSyn
-     * @param minSyn
+     * @param maxTaktikosPer
+     * @param maxEpendyseisPer
+     * @param maxSynoloPer
      * */
     public MinistryStats(
             final List<Ypourgeio> maxTakt,
-            final List<Ypourgeio> minTakt,
             final List<Ypourgeio> maxEpend,
-            final List<Ypourgeio> minEpend,
             final List<Ypourgeio> maxSyn,
-            final List<Ypourgeio> minSyn) {
+            final List<BigDecimal> maxTaktikosPer,
+            final List<BigDecimal> maxEpendyseisPer,
+            final List<BigDecimal> maxSynoloPer) {
 
-        this.maxTaktikos = new ArrayList<>(maxTakt);
-        this.minTaktikos = new ArrayList<>(minTakt);
-        this.maxEpendyseis = new ArrayList<>(maxEpend);
-        this.minEpendyseis = new ArrayList<>(minEpend);
-        this.maxSynolo = new ArrayList<>(maxSyn);
-        this.minSynolo = new ArrayList<>(minSyn);
+        maxTaktikos = new ArrayList<>(maxTakt);
+        maxEpendyseis = new ArrayList<>(maxEpend);
+        maxSynolo = new ArrayList<>(maxSyn);
+        maxTaktikosPercentages = new ArrayList<>(maxTaktikosPer);
+        maxEpendyseisPercentages = new ArrayList<>(maxEpendyseisPer);
+        maxSynoloPercentages = new ArrayList<>(maxSynoloPer);
     }
 
     /**
@@ -57,27 +59,11 @@ public final class MinistryStats {
     }
 
     /**
-     * Getter for min Taktikos.
-     * @return min taktikos
-     * */
-    public List<Ypourgeio> getMinTaktikos() {
-        return new ArrayList<>(minTaktikos);
-    }
-
-    /**
      * Getter for max Ependyseis.
      * @return max ependyseis
      * */
     public List<Ypourgeio> getMaxEpendyseis() {
         return new ArrayList<>(maxEpendyseis);
-    }
-
-    /**
-     * Getter for min Ependyseis.
-     * @return max ependyseis
-     * */
-    public List<Ypourgeio> getMinEpendyseis() {
-        return new ArrayList<>(minEpendyseis);
     }
 
     /**
@@ -89,10 +75,26 @@ public final class MinistryStats {
     }
 
     /**
-     * Getter for min Synolo.
-     * @return max synolo
+     * Getter for max taktikos percentages.
+     * @return max taktikos percentages
      * */
-    public List<Ypourgeio> getMinSynolo() {
-        return new ArrayList<>(minSynolo);
+    public List<BigDecimal> getMaxTaktikosPercentages() {
+        return new ArrayList<>(maxTaktikosPercentages);
+    }
+
+    /**
+     * Getter for max ependyseis percentages.
+     * @return max ependyseis percentages
+     * */
+    public List<BigDecimal> getMaxEpendyseisPercentages() {
+        return new ArrayList<>(maxEpendyseisPercentages);
+    }
+
+     /**
+     * Getter for max synolo percentages.
+     * @return max synolo percentages
+     * */
+    public List<BigDecimal> getMaxSynoloPercentages() {
+        return new ArrayList<>(maxSynoloPercentages);
     }
 }
