6be476216ce6c3cd1751a454c0eb31ba1406b79a
Ilias Pavlidis <t8240112@aueb.gr>
2026-01-09 21:24:35 +0000

    Remove unnecessary import

--- src/test/java/budgetreader/ReadBudgetTest.java
+++ src/test/java/budgetreader/ReadBudgetTest.java
@@ -3,7 +3,6 @@ package budgetreader;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
