0f9863c78d12df16f03ab48e4c255e345baa9e33
Ioanna Tzima <t8240141@aueb.gr>
2026-01-10 11:55:11 +0200

    BUDGET++ in the beginning of CLI

--- src/main/java/DhmosiaBytes/Main.java
+++ src/main/java/DhmosiaBytes/Main.java
@@ -62,6 +62,22 @@ public final class Main {
      */
     public static void main(final String[] args) {
 
+        // ANSI Bold Blue
+        String b = "\u001B[1;34m";
+        String r = "\u001B[0m";
+
+        System.out.println(b + "  ____  _   _ ____   ____ _____ _____   "
+        + "      _          _      ");
+        System.out.println(b + " | __ )| | | |  _ \\ / ___| ____|_   _|  "
+        + "    _| |_      _| |_    ");
+        System.out.println(b + " |  _ \\| | | | | | | |  _|  _|   | |    "
+        + "   |_   _|    |_   _|   ");
+        System.out.println(b + " | |_) | |_| | |_| | |_| | |___  | |    "
+        + "     |_|        |_|     ");
+        System.out.println(b + " |____/ \\___/|____/ \\____|_____| |_|    "
+        + "                        " + r);
+        System.out.println();
+
         Scanner input = new Scanner(System.in, "UTF-8");
         LoginService log = new LoginService();
 
